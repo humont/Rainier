987553907aa8cee8ba1681f5512df8c3fb287fca
Brent Simmons <brent@ranchero.com>
2017-04-30 12:35:31 -0700

    Start Quick Script window and window controller.

---
+++ Frontier/QuickScript/QuickScriptWindowController.swift
@@ -0,0 +1,16 @@
+//
+//  QuickScriptWindowController.swift
+//  Frontier
+//
+//  Created by Brent Simmons on 4/30/17.
+//  Copyright © 2017 Ranchero Software. All rights reserved.
+//
+
+import Cocoa
+
+class QuickScriptWindowController: NSObject {
+
+	
+	@IBAction func runScript(_ sender: Any) {
+	}
+}
