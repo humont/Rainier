e0e54a25b1142a2b66df66e8aa85faf4ec0902d6
Brent Simmons <brent@ranchero.com>
2017-04-30 16:41:22 -0700

    Start bridging the parser. Broke the build again.

---
+++ UserTalk/UserTalk/Compiler/ParserBridge.h
@@ -0,0 +1,13 @@
+//
+//  ParserBridge.h
+//  UserTalk
+//
+//  Created by Brent Simmons on 4/30/17.
+//  Copyright © 2017 Ranchero Software. All rights reserved.
+//
+
+#ifndef ParserBridge_h
+#define ParserBridge_h
+
+
+#endif /* ParserBridge_h */
---
+++ UserTalk/UserTalk/Compiler/langparser.h
@@ -0,0 +1,35 @@
+//
+//  langparser.h
+//  UserTalk
+//
+//  Created by Brent Simmons on 4/30/17.
+//  Copyright © 2017 Ranchero Software. All rights reserved.
+//
+
+#ifndef langparser_h
+#define langparser_h
+
+// OrigFrontier: langparser.h
+
+#define YYSTYPE CodeTreeNode /*data type of yacc stack*/
+
+extern CodeTreeNode *yylval;
+extern CodeTreeNode *yyval;
+
+#define tokentype NSInteger /*something that's returned by parsegettoken*/
+
+
+/*prototypes*/
+
+int yyparse (void); // langparser.c
+
+void parsesetscanstring (Handle, boolean); // langscan.c
+
+unsigned long parsegetscanoffset (unsigned long, unsigned short);
+
+void parsesetscanoffset (unsigned long);
+
+tokentype parsegettoken (CodeTreeNode *);
+
+
+#endif /* langparser_h */
