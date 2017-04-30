9ff2c83588c412447db88bd3cc7b021856be3129
Brent Simmons <brent@ranchero.com>
2017-04-30 12:46:53 -0700

    Start Error Window.

---
+++ Frontier/ErrorWindow/ErrorWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  ErrorWindowController.swift
+//  Frontier
+//
+//  Created by Brent Simmons on 4/30/17.
+//  Copyright © 2017 Ranchero Software. All rights reserved.
+//
+
+import Cocoa
+
+class ErrorWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
