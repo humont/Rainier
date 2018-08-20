288f116b1cdd2a6338219c5e515ba13725bd3632
Brent Simmons <brent@ranchero.com>
2018-08-19 21:19:58 -0700

    Create Rainier project and workspace. Add submodules.

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Rainier
+//
+//  Created by Brent Simmons on 8/19/18.
+//  Copyright © 2018 Ranchero Software, LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
